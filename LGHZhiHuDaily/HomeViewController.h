b41eba72935a7ad5d980bdd81afa0e58db151630
小肥观 <liuguanhua@liuguanhuadeMacBook-Pro.local>
2017-06-20 21:08:22 +0800

    the third version

---
+++ LGHZhiHuDaily/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  LGHZhiHuDaily
+//
+//  Created by 刘观华 on 17/6/20.
+//  Copyright © 2017年 SmoothV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
